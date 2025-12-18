da96c8ec44e592b230a7931831842f571ce1d9f1
Francesco301298 <163843294+Francesco301298@users.noreply.github.com>
2025-12-18 17:33:34 +0100

    backtesting.py

--- core/backtesting.py
+++ core/backtesting.py
@@ -1,1 +1,5 @@
-
+import numpy as np
+import pandas as pd
+from core.metrics import calculate_portfolio_metrics
+from core.optimization import optimize_portfolio_weights
+from core.rebalancing import calculate_portfolio_with_rebalancing
